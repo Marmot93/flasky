e79bcbda18a6d54e630eea9aa8f343fdc5dbb23e
WenfengMou <654332905@qq.com>
2017-08-09 17:16:04 +0800

    8-9 15:42

--- flasky.py
+++ flasky.py
@@ -1,23 +1,49 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
+from forms import NameForm
+
 app = Flask(__name__)
-# database
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-x = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'nhDaXvbs1T-HnaU5kgAdJDHtQnuutXhY7xM2cn_fdMo'
+
 db = SQLAlchemy(app)
+
+bootstrap = Bootstrap(app)
 # manager
 maneger = Manager(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.route('/1/')
+def fun1():
+    return ''
+
+
+@app.route('/2/')
+def fun2():
+    return redirect(url_for('fun1'))
 
 
 if __name__ == '__main__':
